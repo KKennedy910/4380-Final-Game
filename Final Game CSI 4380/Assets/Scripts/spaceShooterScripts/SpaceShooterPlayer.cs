e8e50e29db7e1d000fa4263628bf636c4ff10df4
yousif <43048698+yousifhanani@users.noreply.github.com>
2021-03-17 17:45:22 -0400

    Space Shooter
    Made a rolling background.
    Made player movement.

---
+++ Final Game CSI 4380/Assets/Scripts/spaceShooterScripts/SpaceShooterPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShooterPlayer : MonoBehaviour
+{
+    public float moveSpeed = 12;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(); 
+    }
+
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, -7.7f, 7.7f);
+
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY, -4.55f, 4.55f);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
---
+++ Final Game CSI 4380/Assets/Scripts/spaceShooterScripts/rollingBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rollingBackground : MonoBehaviour
+{
+    public float speed = 0.3f;
+    Material myMaterial;
+    Vector2 offset;
+
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(0, speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
